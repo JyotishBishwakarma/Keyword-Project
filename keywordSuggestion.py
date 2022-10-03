8ccf4ae9c04e53937aada374bac4515274d7f2bf
Jyotish Bishwakarma <jyotish.bishwakarma@outlook.com>
2022-10-03 23:02:11 +0530

    project-skeleton

--- keywordSuggestion.py
+++ keywordSuggestion.py
@@ -1,13 +1,18 @@
 from scraping import amazon_scraping
+from scraping import google_keywords
 from mongoconntest import get_database
+
 import pandas as pd
 import json
+
 def keywordsuggest(word):
     amazon_scraping(word)
+    google_keywords(word)
     mydb = get_database()
     mycol = mydb["testnewkeywrd"]
+    word2 = word.replace("%20"," ")
 
-    myquery = { "value": {"$regex":f"^{word}"} }
+    myquery = { "value": {"$regex":f".*{word2}.*"} }
 
     mydoc = mycol.find(myquery)
     print(f"testmydoc {mydoc}")
@@ -20,7 +25,7 @@ def keywordsuggest(word):
     with open("data/amazon.json","w") as outfile:
         outfile.write(df_json)
 
-keywordsuggest('ma')
+
 
 
 
--- scraping.py
+++ scraping.py
@@ -1,6 +1,8 @@
 import json
 import urllib.request
+import requests
 from mongoconntest import get_database
+import xml.etree.ElementTree as ET
 
 def amazon_scraping(query):
     url=f"https://completion.amazon.ca/api/2017/suggestions?limit=10&prefix={query}&suggestion-type=WIDGET&suggestion-type=KEYWORD&page-type=Search&alias=aps&site-variant=desktop&version=3&event=onkeypress&wc=&lop=en_CA&last-prefix=c&avg-ks-time=2696&fb=1&session-id=131-7867665-8023625&request-id=GH37EMG5ABHERT1VEZXV&mid=A2EUQ1WTGCTBG2&plain-mid=7&client-info=amazon-search-ui"
@@ -22,6 +24,35 @@ def amazon_scraping(query):
             pass
         
 
-amazon_scraping('ab')
+
+# Referred from 
+# https://dev.to/hassancs91/develop-a-keyword-research-tool-in-minutes-python-and-c-4g32
+
+def google_keywords(word):
+        mydb = get_database()
+        mycol = mydb["testnewkeywrd"]
+        questionResults ={}
+       
+        # API Call
+        googleSearchUrl = f"http://google.com/complete/search?output=toolbar&gl=COUNTRY&q={word}"
+
+        # Call The URL and Read Data
+        result = requests.get(googleSearchUrl)
+        tree = ET.ElementTree(ET.fromstring(result.content))
+        root = tree.getroot()
+        
+
+        for suggestion in root.findall('CompleteSuggestion'):
+            question = suggestion.find('suggestion').attrib.get('data')
+            print(question)
+            questionResults["_id"] = question
+            questionResults["value"] = question
+            try:
+                mycol.insert_one(questionResults)
+            except:
+                pass
+
+
+
 
 
